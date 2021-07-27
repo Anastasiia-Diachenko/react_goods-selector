ae674a5c528f9ba383d8a4f28eb3738ea93edd85
Anastasiia Diachenko <anastasiiadiachenko1@gmail.com>
2021-07-27 13:40:49 +0300

    Updates

--- precache-manifest.ea20e5d5e15acdb391aac8b1bcdcf423.js
+++ precache-manifest.ea20e5d5e15acdb391aac8b1bcdcf423.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6910e3f664aeab140dc8897f85b14e19",
+    "revision": "e5c9f33c9604286be52ccf0144625220",
     "url": "./index.html"
   },
   {
-    "revision": "f575ba20dff2416d0c1e",
+    "revision": "437ac44434c504068348",
     "url": "./static/css/main.2b6080bd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.8faab883.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f575ba20dff2416d0c1e",
-    "url": "./static/js/main.a32bf064.chunk.js"
+    "revision": "437ac44434c504068348",
+    "url": "./static/js/main.930b0b81.chunk.js"
   },
   {
     "revision": "f861d31e3669d4c8fef0",
